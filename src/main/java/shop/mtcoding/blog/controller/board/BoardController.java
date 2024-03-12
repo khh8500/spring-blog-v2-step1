83f720e0a324f5601de3a9e178bb3af6be8448c2
khh <khh8500@naver.com>
2024-03-12 19:26:38 +0900

    게시글 목록보기

--- src/main/java/shop/mtcoding/blog/controller/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/controller/board/BoardController.java
@@ -1,10 +1,13 @@
 package shop.mtcoding.blog.controller.board;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
@@ -18,7 +21,9 @@ public class BoardController {
     }
 
     @GetMapping("/" )
-    public String index() {
+    public String index(HttpServletRequest request) {
+        List<Board> boardList = boardNativeRepository.findAll();
+        request.setAttribute("boardList", boardList);
         return "index";
     }
 
